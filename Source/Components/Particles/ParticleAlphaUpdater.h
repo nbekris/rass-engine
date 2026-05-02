53facb1173d14e71f92111f5ccc070987b4bee64
Taro Omiya <japtar10101@duck.com>
2026-05-02 05:58:17 -0700

    Updating `ParticleAlphaUpdater` so it'll compile.

--- Source/Components/Particles/ParticleAlphaUpdater.h
+++ Source/Components/Particles/ParticleAlphaUpdater.h
@@ -1,107 +1,56 @@
-//------------------------------------------------------------------------------
+// File Name:    ParticleAlphaUpdater.h
+// Author(s):    main Taro Omiya, secondary Steven Yacoub, Niko Bekris, Eric Fleegal
+// Course:       GAM541
+// Project:      RASS
+// Purpose:      File stream utilities for reading and writing data.
 //
-// File Name:	ParticleAlphaUpdater.h
-// Author(s):	taro.omiya
-// Course:		CS529F25
-// Project:		Project 7
-// Purpose:		Updates the particle's alpha (opacity)
-//
-// Copyright © 2025 DigiPen (USA) Corporation.
-//
-//------------------------------------------------------------------------------
+// Copyright © 2026 DigiPen (USA) Corporation.
 
 #pragma once
 
-//------------------------------------------------------------------------------
-// Includes:
-//------------------------------------------------------------------------------
-
 #include "Component.h"
+#include "Cloneable.h"
+#include "Events/GlobalEventArgs.h"
+#include "Events/GlobalEventListener.h"
+#include "IEvent.h"
 #include "TweenCurve.h"
 
-//------------------------------------------------------------------------------
-// External Declarations:
-//------------------------------------------------------------------------------
-
-//------------------------------------------------------------------------------
-// Namespace Declarations:
-//------------------------------------------------------------------------------
+// Forward Declarations:
+namespace RassEngine {
+class Stream;
+}
 
 namespace RassEngine::Components::Particles {
 // Forward Declarations:
-class Stream;
-class Particle;
-
-// Typedefs:
+class ParticleManager;
 
 // Class Definition:
-class ParticleAlphaUpdater : public Component {
-	// Public Constants and Enums:
-public:
-	static const char *NAME;
-
-	// Constructors/Destructors:
+class ParticleAlphaUpdater : public Cloneable<Component, ParticleAlphaUpdater> {
 public:
 	ParticleAlphaUpdater(void);
 
 	// @brief This copy-constructor should perform a shallow copy of the data.
-	ParticleAlphaUpdater(const ParticleAlphaUpdater *other);
-
-	~ParticleAlphaUpdater(void) override {};
-
-	// Public Static Functions:
-public:
-
-	// Public Functions:
-public:
-	ParticleAlphaUpdater *Clone() const override {
-		return new ParticleAlphaUpdater(this);
-	}
+	ParticleAlphaUpdater(const ParticleAlphaUpdater &other);
+	virtual ~ParticleAlphaUpdater(void) override;
 
 	// @brief Initialize the component.
-	// @brief [NOTE: Called when a new entity is initialized after creation.]
-	// @brief [NOTE: Many components won't require this step; others might.]  
-	//
 	// @return bool = true if initialization successful, otherwise false.
-	bool Initialize() override {
-		return true;
-	};
+	bool Initialize() override;
 
-	// @brief Update the component each frame.
-	//
-	// @param dt = Delta time (in seconds) of the last frame.
-	void Update(float dt) override;
+	// Inherited via Cloneable
+	const std::string_view &NameClass() const override;
 
-	// @brief Render the component each frame.
-	// @brief [NOTE: Modern engines handle rendering in a more complicated way.]
-	// @brief [NOTE: Some components are rendered only when debug drawing is enabled.]
-	void Render() const override {};
-
-	// @brief Read the properties of a ParticleAlphaUpdater component from a stream.
-	// @brief Specific Steps:
-	// @brief   Check for valid stream (optional).
-	// @brief   Traverse down the tree to the "ParticleAlphaUpdater" object (PushNode).
-	// @brief   Read component-specific data here.
-	// @brief   Return to the original location in the tree (PopObject).
-	//
+	// @brief Read the properties of a ParticleMover component from a stream.
 	// @param stream = The data stream used for reading.
-	void Read(Stream &stream) override;
-
-	// Public Event Handlers
-public:
+	bool Read(Stream &stream) override;
 
 	// Private Functions:
 private:
+	bool Update(const IEvent<Events::GlobalEventArgs> *, const Events::GlobalEventArgs &);
 
-	// Private Constants:
-private:
-
-	// Private Static Variables:
-private:
-
-	// Private Variables:
-private:
+	Events::GlobalEventListener<ParticleAlphaUpdater> updateListener;
 	TweenCurve curve{};
+	ParticleManager *manager = nullptr;
 };
 
 }	// namespace
