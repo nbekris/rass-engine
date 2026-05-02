5b2cadce662e25216a6f9a8b80bbe7c615242650
Taro Omiya <japtar10101@duck.com>
2026-05-02 05:48:28 -0700

    Updating `ParticleSizeUpdater` so it'll compile.

--- Source/Components/Particles/ParticleSizeUpdater.cpp
+++ Source/Components/Particles/ParticleSizeUpdater.cpp
@@ -1,126 +1,108 @@
-//------------------------------------------------------------------------------
+// File Name:    ParticleSizeUpdater.cpp
+// Author(s):    main Taro Omiya, secondary Steven Yacoub, Niko Bekris, Eric Fleegal
+// Course:       GAM541
+// Project:      RASS
+// Purpose:      File stream utilities for reading and writing data.
 //
-// File Name:	ParticleSizeUpdater.cpp
-// Author(s):	taro.omiya
-// Course:		CS529F25
-// Project:		Project 7
-// Purpose:		Updates the particle's size
-//
-// Copyright © 2025 DigiPen (USA) Corporation.
-//
-//------------------------------------------------------------------------------
-
-//------------------------------------------------------------------------------
-// Includes:
-//------------------------------------------------------------------------------
+// Copyright © 2026 DigiPen (USA) Corporation.
 
 #include "Precompiled.h"
 #include "ParticleSizeUpdater.h"
+
+#include <glm/vec3.hpp>
+
 #include "Component.h"
-#include "Particle.h"
-#include "ParticleEmitter.h"
+#include "Entity.h"
+#include "Events/Global.h"
+#include "ParticleManager.h"
+#include "Stream.h"
+#include "Systems/GlobalEvents/IGlobalEventsSystem.h"
+#include "Systems/Logging/ILoggingSystem.h"
+#include "Systems/Time/ITimeSystem.h"
 #include "Utils.h"
 
-//------------------------------------------------------------------------------
-// External Declarations:
-//------------------------------------------------------------------------------
-
-//------------------------------------------------------------------------------
-// Namespace Declarations:
-//------------------------------------------------------------------------------
+using namespace RassEngine::Systems;
+using namespace RassEngine::Events;
+using namespace RassEngine::Graphics;
 
 namespace RassEngine::Components::Particles {
-//--------------------------------------------------------------------------
-// Public Constants:
-//--------------------------------------------------------------------------
-
-//--------------------------------------------------------------------------
-// Public Static Variables:
-//--------------------------------------------------------------------------
-
-const char *ParticleSizeUpdater::NAME = "ParticleSizeUpdater";
-static const char *CURVE = "Curve";
-
-//--------------------------------------------------------------------------
-// Public Variables:
-//--------------------------------------------------------------------------
-
-//--------------------------------------------------------------------------
-// Private Static Constants:
-//--------------------------------------------------------------------------
-
-//--------------------------------------------------------------------------
-// Private Constants:
-//--------------------------------------------------------------------------
-
-//--------------------------------------------------------------------------
-// Private Static Variables:
-//--------------------------------------------------------------------------
-
-//--------------------------------------------------------------------------
-// Private Variables:
-//--------------------------------------------------------------------------
-
-//--------------------------------------------------------------------------
-// Constructors/Destructors:
-//--------------------------------------------------------------------------
 
 #pragma region Constructors
 
 ParticleSizeUpdater::ParticleSizeUpdater(void)
-	: Component() {}
+	: Cloneable<Component, ParticleSizeUpdater>()
+	, updateListener{this, &ParticleSizeUpdater::Update}
+{}
 
-ParticleSizeUpdater::ParticleSizeUpdater(const ParticleSizeUpdater *other)
-	: Component(other), curve(other->curve) {}
+ParticleSizeUpdater::ParticleSizeUpdater(const ParticleSizeUpdater &other)
+	: Cloneable<Component, ParticleSizeUpdater>(other)
+	, updateListener{this, &ParticleSizeUpdater::Update}
+{}
+
+ParticleSizeUpdater::~ParticleSizeUpdater(void) {
+	if(IGlobalEventsSystem::Get() == nullptr) {
+		return;
+	}
+
+	// Unbind from the event
+	IGlobalEventsSystem::Get()->unbind(Global::Update, &updateListener);
+}
 
 #pragma endregion Constructors
 
 //--------------------------------------------------------------------------
-// Public Static Functions:
+// Public Functions:
 //--------------------------------------------------------------------------
-
-#pragma region Public Static Functions
-
-#pragma endregion Public Static Functions
-
-	//--------------------------------------------------------------------------
-	// Public Functions:
-	//--------------------------------------------------------------------------
 
 #pragma region Public Functions
 
-void ParticleSizeUpdater::Update(float dt) {
-	ParticleManager *manager = Utils::GetComponentSafe<ParticleManager>(Parent(), ParticleManager::NAME, NAME);
-	if(manager == nullptr) {
-		return;
+bool ParticleSizeUpdater::Initialize() {
+	// Retrieve the particle manager
+	if(Parent() == nullptr) {
+		LOG_ERROR("Cannot resize particles: {} is not attached to an {}", NameClass(), NAMEOF(Entity));
+		return false;
 	}
 
-	// Adjust the scale for each particle
-	manager->ForEachActiveParticle([this, dt] (const ParticleManager::StartingStats &startingStats, Particle &particle) {
-		// Compute time
-		float scalar = 1.f - particle.lifetime / startingStats.lifetime;
-		scalar = curve.Calculate(scalar);
+	manager = Parent()->Get<ParticleManager>();
+	if(manager == nullptr) {
+		LOG_ERROR("Cannot resize particles: {} is not attached to an {}", NAMEOF(ParticleManager), NAMEOF(Entity));
+		return false;
+	}
 
-		// Update the particle's scale
-		Vector2D newScale = startingStats.scale;
-		newScale.Scale(scalar);
-		particle.scale = newScale;
-		});
+	// Make sure all systems are available
+	if(IGlobalEventsSystem::Get() == nullptr) {
+		LOG_ERROR("Cannot resize particles: {} is not registered", NAMEOF(Systems::IGlobalEventsSystem));
+		return false;
+	}
+	if(ITimeSystem::Get() == nullptr) {
+		LOG_ERROR("Cannot resize particles: {} is not registered", NAMEOF(Systems::ITimeSystem));
+		return false;
+	}
+
+	// Bind to events
+	IGlobalEventsSystem::Get()->bind(Global::Update, &updateListener);
+	return true;
 }
 
-void ParticleSizeUpdater::Read(Stream &stream) {
+const std::string_view &ParticleSizeUpdater::NameClass() const {
+	static constexpr std::string_view className = NAMEOF(RassEngine::Components::Particles::ParticleSizeUpdater);
+	return className;
+}
+
+bool ParticleSizeUpdater::Read(Stream &stream) {
 	// Make sure stream is valid
-	if(!Utils::IsStreamVerified(stream, NAME, NAME)) {
-		return;
+	if(!Component::Read(stream)) {
+		return false;
 	}
 
 	// Read the node values
-	stream.PushNode(NAME);
+	stream.PushNode(NAMEOF(ParticleSizeUpdater));
 
 	// Read the curve attributes
 	curve.Read(stream);
 
 	stream.PopNode();
+	return true;
 }
 
 #pragma endregion Public Functions
@@ -130,6 +112,21 @@ void ParticleSizeUpdater::Read(Stream &stream) {
 //--------------------------------------------------------------------------
 
 #pragma region Private Functions
+
+bool ParticleSizeUpdater::Update(const IEvent<GlobalEventArgs> *, const GlobalEventArgs &) {
+	float dt = ITimeSystem::Get()->GetDeltaTimeSec();
+
+	// Adjust the scale for each particle
+	manager->ForEachActiveParticle([this, dt] (const ParticleManager::StartingStats &startingStats, Particle &particle) {
+		// Compute time
+		float scalar = 1.f - particle.lifetime / startingStats.lifetime;
+		scalar = curve.Calculate(scalar);
+
+		// Update the particle's scale
+		particle.scale = startingStats.scale * scalar;
+	});
+	return true;
+}
 
 #pragma endregion Private Functions
 
--- Source/Components/Particles/ParticleSizeUpdater.h
+++ Source/Components/Particles/ParticleSizeUpdater.h
@@ -1,107 +1,56 @@
-//------------------------------------------------------------------------------
+// File Name:    ParticleSizeUpdater.h
+// Author(s):    main Taro Omiya, secondary Steven Yacoub, Niko Bekris, Eric Fleegal
+// Course:       GAM541
+// Project:      RASS
+// Purpose:      File stream utilities for reading and writing data.
 //
-// File Name:	ParticleSizeUpdater.h
-// Author(s):	taro.omiya
-// Course:		CS529F25
-// Project:		Project 7
-// Purpose:		Updates the particle's sizes
-//
-// Copyright © 2025 DigiPen (USA) Corporation.
-//
-//------------------------------------------------------------------------------
+// Copyright © 2026 DigiPen (USA) Corporation.
 
 #pragma once
 
-//------------------------------------------------------------------------------
-// Includes:
-//------------------------------------------------------------------------------
-
 #include "Component.h"
+#include "Cloneable.h"
+#include "Events/GlobalEventArgs.h"
+#include "Events/GlobalEventListener.h"
+#include "IEvent.h"
 #include "TweenCurve.h"
 
-//------------------------------------------------------------------------------
-// External Declarations:
-//------------------------------------------------------------------------------
-
-//------------------------------------------------------------------------------
-// Namespace Declarations:
-//------------------------------------------------------------------------------
+// Forward Declarations:
+namespace RassEngine {
+class Stream;
+}
 
 namespace RassEngine::Components::Particles {
 // Forward Declarations:
-class Stream;
-class Particle;
-
-// Typedefs:
+class ParticleManager;
 
 // Class Definition:
-class ParticleSizeUpdater : public Component {
-	// Public Constants and Enums:
-public:
-	static const char *NAME;
-
-	// Constructors/Destructors:
+class ParticleSizeUpdater : public Cloneable<Component, ParticleSizeUpdater> {
 public:
 	ParticleSizeUpdater(void);
 
 	// @brief This copy-constructor should perform a shallow copy of the data.
-	ParticleSizeUpdater(const ParticleSizeUpdater *other);
-
-	~ParticleSizeUpdater(void) override {};
-
-	// Public Static Functions:
-public:
-
-	// Public Functions:
-public:
-	ParticleSizeUpdater *Clone() const override {
-		return new ParticleSizeUpdater(this);
-	}
+	ParticleSizeUpdater(const ParticleSizeUpdater &other);
+	virtual ~ParticleSizeUpdater(void) override;
 
 	// @brief Initialize the component.
-	// @brief [NOTE: Called when a new entity is initialized after creation.]
-	// @brief [NOTE: Many components won't require this step; others might.]  
-	//
 	// @return bool = true if initialization successful, otherwise false.
-	bool Initialize() override {
-		return true;
-	};
+	bool Initialize() override;
 
-	// @brief Update the component each frame.
-	//
-	// @param dt = Delta time (in seconds) of the last frame.
-	void Update(float dt) override;
+	// Inherited via Cloneable
+	const std::string_view &NameClass() const override;
 
-	// @brief Render the component each frame.
-	// @brief [NOTE: Modern engines handle rendering in a more complicated way.]
-	// @brief [NOTE: Some components are rendered only when debug drawing is enabled.]
-	void Render() const override {};
-
-	// @brief Read the properties of a ParticleSizeUpdater component from a stream.
-	// @brief Specific Steps:
-	// @brief   Check for valid stream (optional).
-	// @brief   Traverse down the tree to the "ParticleSizeUpdater" object (PushNode).
-	// @brief   Read component-specific data here.
-	// @brief   Return to the original location in the tree (PopObject).
-	//
+	// @brief Read the properties of a ParticleMover component from a stream.
 	// @param stream = The data stream used for reading.
-	void Read(Stream &stream) override;
-
-	// Public Event Handlers
-public:
+	bool Read(Stream &stream) override;
 
 	// Private Functions:
 private:
+	bool Update(const IEvent<Events::GlobalEventArgs> *, const Events::GlobalEventArgs &);
 
-	// Private Constants:
-private:
-
-	// Private Static Variables:
-private:
-
-	// Private Variables:
-private:
+	Events::GlobalEventListener<ParticleSizeUpdater> updateListener;
 	TweenCurve curve{};
+	ParticleManager *manager = nullptr;
 };
 
 }	// namespace
