bcdd05c168f49dee145b00475c8e31638b04594b
Taro Omiya <japtar10101@duck.com>
2026-05-02 05:31:19 -0700

    Updating `ParticleMover` to actually compile.

--- Source/Components/Particles/ParticleMover.cpp
+++ Source/Components/Particles/ParticleMover.cpp
@@ -1,119 +1,105 @@
-//------------------------------------------------------------------------------
+// File Name:    ParticleMover.cpp
+// Author(s):    main Taro Omiya, secondary Steven Yacoub, Niko Bekris, Eric Fleegal
+// Course:       GAM541
+// Project:      RASS
+// Purpose:      File stream utilities for reading and writing data.
 //
-// File Name:	ParticleMover.cpp
-// Author(s):	japta
-// Course:		CS529F25
-// Project:		Project 4
-// Purpose:		This component class is responsible for ...
-//
-// Copyright © 2025 DigiPen (USA) Corporation.
-//
-//------------------------------------------------------------------------------
-
-//------------------------------------------------------------------------------
-// Includes:
-//------------------------------------------------------------------------------
+// Copyright © 2026 DigiPen (USA) Corporation.
 
 #include "Precompiled.h"
 #include "ParticleMover.h"
+
+#include <glm/vec3.hpp>
+
 #include "Component.h"
-#include "Stream.h"
-#include "Utils.h"
+#include "Entity.h"
+#include "Events/Global.h"
 #include "ParticleManager.h"
+#include "Stream.h"
+#include "Systems/GlobalEvents/IGlobalEventsSystem.h"
+#include "Systems/Logging/ILoggingSystem.h"
+#include "Systems/Time/ITimeSystem.h"
+#include "Utils.h"
 
-//------------------------------------------------------------------------------
-// External Declarations:
-//------------------------------------------------------------------------------
-
-//------------------------------------------------------------------------------
-// Namespace Declarations:
-//------------------------------------------------------------------------------
+using namespace RassEngine::Systems;
+using namespace RassEngine::Events;
+using namespace RassEngine::Graphics;
 
 namespace RassEngine::Components::Particles {
-//--------------------------------------------------------------------------
-// Public Constants:
-//--------------------------------------------------------------------------
-
-//--------------------------------------------------------------------------
-// Public Static Variables:
-//--------------------------------------------------------------------------
-
-const char *ParticleMover::NAME = "ParticleMover";
-
-//--------------------------------------------------------------------------
-// Public Variables:
-//--------------------------------------------------------------------------
-
-//--------------------------------------------------------------------------
-// Private Static Constants:
-//--------------------------------------------------------------------------
-
-//--------------------------------------------------------------------------
-// Private Constants:
-//--------------------------------------------------------------------------
-
-//--------------------------------------------------------------------------
-// Private Static Variables:
-//--------------------------------------------------------------------------
-
-//--------------------------------------------------------------------------
-// Private Variables:
-//--------------------------------------------------------------------------
-
-//--------------------------------------------------------------------------
-// Constructors/Destructors:
-//--------------------------------------------------------------------------
 
 #pragma region Constructors
 
 ParticleMover::ParticleMover(void)
-	: Component() {}
+	: Cloneable<Component, ParticleMover>()
+	, updateListener{this, &ParticleMover::Update}
+{}
 
-ParticleMover::ParticleMover(const ParticleMover *other)
-	: Component(other) {}
+ParticleMover::ParticleMover(const ParticleMover &other)
+	: Cloneable<Component, ParticleMover>(other)
+	, updateListener{this, &ParticleMover::Update}
+{}
+
+ParticleMover::~ParticleMover(void) {
+	if(IGlobalEventsSystem::Get() == nullptr) {
+		return;
+	}
+
+	// Unbind from the event
+	IGlobalEventsSystem::Get()->unbind(Global::Update, &updateListener);
+}
 
 #pragma endregion Constructors
 
 //--------------------------------------------------------------------------
-// Public Static Functions:
+// Public Functions:
 //--------------------------------------------------------------------------
-
-#pragma region Public Static Functions
-
-#pragma endregion Public Static Functions
-
-	//--------------------------------------------------------------------------
-	// Public Functions:
-	//--------------------------------------------------------------------------
 
 #pragma region Public Functions
 
-void ParticleMover::Update(float dt) {
-	ParticleManager *manager = Utils::GetComponentSafe<ParticleManager>(Parent(), ParticleManager::NAME, NAME);
-	if(manager == nullptr) {
-		return;
+bool ParticleMover::Initialize() {
+	// Retrieve the particle manager
+	if(Parent() == nullptr) {
+		LOG_ERROR("Cannot move particles: {} is not attached to an {}", NameClass(), NAMEOF(Entity));
+		return false;
 	}
 
-	// Adjust the transform for each particle
-	manager->ForEachActiveParticle([this, dt] (const ParticleManager::StartingStats &startingStats, Particle &particle) {
-		// Update the position of the active particle.
-		particle.position.ScaleAdd(dt, startingStats.velocity);
+	manager = Parent()->Get<ParticleManager>();
+	if(manager == nullptr) {
+		LOG_ERROR("Cannot move particles: {} is not attached to an {}", NAMEOF(ParticleManager), NAMEOF(Entity));
+		return false;
+	}
 
-		// Update the rotation of the active particle.
-		particle.rotationRad += dt * startingStats.angularVelocityRad;
-		});
+	// Make sure all systems are available
+	if(IGlobalEventsSystem::Get() == nullptr) {
+		LOG_ERROR("Cannot move particles: {} is not registered", NAMEOF(Systems::IGlobalEventsSystem));
+		return false;
+	}
+	if(ITimeSystem::Get() == nullptr) {
+		LOG_ERROR("Cannot move particles: {} is not registered", NAMEOF(Systems::ITimeSystem));
+		return false;
+	}
+
+	// Bind to events
+	IGlobalEventsSystem::Get()->bind(Global::Update, &updateListener);
+	return true;
 }
 
-void ParticleMover::Read(Stream &stream) {
+const std::string_view &ParticleMover::NameClass() const {
+	static constexpr std::string_view className = NAMEOF(RassEngine::Components::Particles::ParticleMover);
+	return className;
+}
+
+bool ParticleMover::Read(Stream &stream) {
 	// Make sure stream is valid
-	if(!Utils::IsStreamVerified(stream, NAME, NAME)) {
-		return;
+	if(!Component::Read(stream)) {
+		return false;
 	}
 
 	// Read the node values
-	stream.PushNode(NAME);
+	stream.PushNode(NAMEOF(ParticleMover));
 
 	stream.PopNode();
+	return true;
 }
 
 #pragma endregion Public Functions
@@ -123,6 +109,20 @@ void ParticleMover::Read(Stream &stream) {
 //--------------------------------------------------------------------------
 
 #pragma region Private Functions
+
+bool ParticleMover::Update(const IEvent<GlobalEventArgs> *, const GlobalEventArgs &) {
+	float dt = ITimeSystem::Get()->GetDeltaTimeSec();
+
+	// Adjust the transform for each particle
+	manager->ForEachActiveParticle([this, dt] (const ParticleManager::StartingStats &startingStats, Particle &particle) {
+		// Update the position of the active particle.
+		particle.position += startingStats.velocity * dt;
+
+		// Update the rotation of the active particle.
+		particle.rotationRad += startingStats.angularVelocityRad * dt;
+	});
+	return true;
+}
 
 #pragma endregion Private Functions
 
--- Source/Components/Particles/ParticleMover.h
+++ Source/Components/Particles/ParticleMover.h
@@ -1,105 +1,54 @@
-//------------------------------------------------------------------------------
+// File Name:    ParticleMover.h
+// Author(s):    main Taro Omiya, secondary Steven Yacoub, Niko Bekris, Eric Fleegal
+// Course:       GAM541
+// Project:      RASS
+// Purpose:      File stream utilities for reading and writing data.
 //
-// File Name:	ParticleMover.h
-// Author(s):	japta
-// Course:		CS529F25
-// Project:		Project 4
-// Purpose:		This component class is responsible for ...
-//
-// Copyright © 2025 DigiPen (USA) Corporation.
-//
-//------------------------------------------------------------------------------
+// Copyright © 2026 DigiPen (USA) Corporation.
 
 #pragma once
 
-//------------------------------------------------------------------------------
-// Includes:
-//------------------------------------------------------------------------------
-
 #include "Component.h"
-#include "Particle.h"
+#include "Cloneable.h"
+#include "Events/GlobalEventArgs.h"
+#include "Events/GlobalEventListener.h"
+#include "IEvent.h"
 
-//------------------------------------------------------------------------------
-// External Declarations:
-//------------------------------------------------------------------------------
-
-//------------------------------------------------------------------------------
-// Namespace Declarations:
-//------------------------------------------------------------------------------
+// Forward Declarations:
+namespace RassEngine {
+class Stream;
+}
 
 namespace RassEngine::Components::Particles {
 // Forward Declarations:
-class Stream;
-
-// Typedefs:
+class ParticleManager;
 
 // Class Definition:
-class ParticleMover : public Component {
-	// Public Constants and Enums:
-public:
-	static const char *NAME;
-
-	// Constructors/Destructors:
+class ParticleMover : public Cloneable<Component, ParticleMover> {
 public:
 	ParticleMover(void);
 
 	// @brief This copy-constructor should perform a shallow copy of the data.
-	ParticleMover(const ParticleMover *other);
-
-	~ParticleMover(void) override {};
-
-	// Public Static Functions:
-public:
-
-	// Public Functions:
-public:
-	ParticleMover *Clone() const override {
-		return new ParticleMover(this);
-	}
+	ParticleMover(const ParticleMover &other);
+	virtual ~ParticleMover(void) override;
 
 	// @brief Initialize the component.
-	// @brief [NOTE: Called when a new entity is initialized after creation.]
-	// @brief [NOTE: Many components won't require this step; others might.]  
-	//
 	// @return bool = true if initialization successful, otherwise false.
-	bool Initialize() override {
-		return true;
-	};
+	bool Initialize() override;
 
-	// @brief Update the component each frame.
-	//
-	// @param dt = Delta time (in seconds) of the last frame.
-	void Update(float dt) override;
-
-	// @brief Render the component each frame.
-	// @brief [NOTE: Modern engines handle rendering in a more complicated way.]
-	// @brief [NOTE: Some components are rendered only when debug drawing is enabled.]
-	void Render() const override {};
+	// Inherited via Cloneable
+	const std::string_view &NameClass() const override;
 
 	// @brief Read the properties of a ParticleMover component from a stream.
-	// @brief Specific Steps:
-	// @brief   Check for valid stream (optional).
-	// @brief   Traverse down the tree to the "ParticleMover" object (PushNode).
-	// @brief   Read component-specific data here.
-	// @brief   Return to the original location in the tree (PopObject).
-	//
 	// @param stream = The data stream used for reading.
-	void Read(Stream &stream) override;
-
-	// Public Event Handlers
-public:
+	bool Read(Stream &stream) override;
 
 	// Private Functions:
 private:
+	bool Update(const IEvent<Events::GlobalEventArgs> *, const Events::GlobalEventArgs &);
 
-	// Private Constants:
-private:
-
-	// Private Static Variables:
-private:
-
-	// Private Variables:
-private:
+	Events::GlobalEventListener<ParticleMover> updateListener;
+	ParticleManager *manager = nullptr;
 };
 
 }	// namespace
