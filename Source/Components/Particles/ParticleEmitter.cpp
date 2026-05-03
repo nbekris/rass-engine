0f29ac9bd2cc50c550d21b093f0460d653a3458e
Taro Omiya <japtar10101@duck.com>
2026-05-03 00:01:06 -0700

    Making `ParticleEmiiter` compilable.

--- Source/Components/Particles/ParticleEmitter.cpp
+++ Source/Components/Particles/ParticleEmitter.cpp
@@ -1,142 +1,189 @@
-//------------------------------------------------------------------------------
+// File Name:    ParticleEmitter.cpp
+// Author(s):    main Taro Omiya, secondary Steven Yacoub, Niko Bekris, Eric Fleegal
+// Course:       GAM541
+// Project:      RASS
+// Purpose:      File stream utilities for reading and writing data.
 //
-// File Name:	ParticleEmitter.cpp
-// Author(s):	dschilling
-// Course:		CS529F25
-// Project:		Project X
-// Purpose:		This component class is responsible for emitting and managing particles.
-//
-// Copyright © 2025 DigiPen (USA) Corporation.
-//
-//------------------------------------------------------------------------------
-
-//------------------------------------------------------------------------------
-// Includes:
-//------------------------------------------------------------------------------
+// Copyright © 2026 DigiPen (USA) Corporation.---------------------------------------------------------------------------
 
 #include "Precompiled.h"
-#include <numbers>
+#include "ParticleEmitter.h"
+
+#include <glm/vec3.hpp>
+#include <glm/vec4.hpp>
+#include <memory>
 #include <string>
+#include <string_view>
+#include <tuple>
 #include <vector>
 
-#include "DGL.h"
-#include <dragon/assert.h>
-
-#include "ParticleEmitter.h"
-#include "Color.h"
+#include "Cloneable.h"
 #include "Component.h"
+#include "Components/Transform.h"
+#include "EmitterShape.h"
 #include "Entity.h"
+#include "Events/Global.h"
+#include "Events/GlobalEventArgs.h"
+#include "IEvent.h"
 #include "Particle.h"
 #include "ParticleManager.h"
 #include "Random.h"
 #include "Stream.h"
-#include "Transform.h"
-#include "Vector2D.h"
-#include "ObjectFactory.h"
-#include "Utils.h"
-#include "LoggingSystem.h"
+#include "Systems/Component/IComponentFactory.h"
+#include "Systems/GlobalEvents/IGlobalEventsSystem.h"
+#include "Systems/Logging/ILoggingSystem.h"
+#include "Systems/Time/ITimeSystem.h"
 #include "TweenCurve.h"
+#include "Utils.h"
 
-//------------------------------------------------------------------------------
-// External Declarations:
-//------------------------------------------------------------------------------
-
-//------------------------------------------------------------------------------
-// Namespace Declarations:
-//------------------------------------------------------------------------------
+using namespace RassEngine::Systems;
+using namespace RassEngine::Events;
+using namespace RassEngine::Graphics;
 
 namespace RassEngine::Components::Particles {
-//--------------------------------------------------------------------------
-// Public Constants:
-//--------------------------------------------------------------------------
-
-//--------------------------------------------------------------------------
-// Public Static Variables:
-//--------------------------------------------------------------------------
-
-//--------------------------------------------------------------------------
-// Public Variables:
-//--------------------------------------------------------------------------
-
-//--------------------------------------------------------------------------
-// Private Static Constants:
-//--------------------------------------------------------------------------
 
 static const char *PARTICLE_EMITTER = "ParticleEmitter";
 static const char *LOG_TRANSFORM = "transform";
 static const char *LOG_MANAGER = "ParticleManager";
 
-//--------------------------------------------------------------------------
-// Private Constants:
-//--------------------------------------------------------------------------
-
-//--------------------------------------------------------------------------
-// Private Static Variables:
-//--------------------------------------------------------------------------
-
-//--------------------------------------------------------------------------
-// Private Variables:
-//--------------------------------------------------------------------------
-
-//--------------------------------------------------------------------------
-// Constructors/Destructors:
-//--------------------------------------------------------------------------
-
 #pragma region Constructors
 
 ParticleEmitter::ParticleEmitter(void)
-	: Component() {}
+	: Cloneable<Component, ParticleEmitter>()
+	, updateListener{this, &ParticleEmitter::Update}
+{}
 
-ParticleEmitter::ParticleEmitter(const ParticleEmitter *other)
-	: Component(other)
-	, emitRate(other->emitRate)
-	, lifetimeMin(other->lifetimeMin)
-	, lifetimeMax(other->lifetimeMax)
-	, scaleMin(other->scaleMin)
-	, scaleMax(other->scaleMax)
-	, tintColor(other->tintColor)
-	, isEmitting(other->isEmitting)
-	, shape(other->shape) {}
+ParticleEmitter::ParticleEmitter(const ParticleEmitter &other)
+	: Cloneable<Component, ParticleEmitter>{other}
+	, emitRate(other.emitRate)
+	, lifetimeMin(other.lifetimeMin)
+	, lifetimeMax(other.lifetimeMax)
+	, scaleMin(other.scaleMin)
+	, scaleMax(other.scaleMax)
+	, tintColor(other.tintColor)
+	, isEmitting(other.isEmitting)
+	, updateListener{this, &ParticleEmitter::Update}
+{}
+
+ParticleEmitter::~ParticleEmitter(void) {
+	if(IGlobalEventsSystem::Get() == nullptr) {
+		return;
+	}
+
+	// Unbind from the event
+	IGlobalEventsSystem::Get()->unbind(Global::Update, &updateListener);
+}
 
 #pragma endregion Constructors
 
-//--------------------------------------------------------------------------
-// Public Static Functions:
-//--------------------------------------------------------------------------
-
-#pragma region Public Static Functions
-
-#pragma endregion Public Static Functions
-
-	//--------------------------------------------------------------------------
-	// Public Functions:
-	//--------------------------------------------------------------------------
-
 #pragma region Public Functions
 
-#pragma endregion Public Functions
-
-	//--------------------------------------------------------------------------
-	// Private Functions:
-	//--------------------------------------------------------------------------
-
-#pragma region Private Functions
-
 bool ParticleEmitter::Initialize() {
+	// Retrieve the particle manager
+	if(Parent() == nullptr) {
+		LOG_ERROR("Cannot emit particles: {} is not attached to an {}", NameClass(), NAMEOF(Entity));
+		return false;
+	}
+
+	manager = Parent()->Get<ParticleManager>();
+	if(manager == nullptr) {
+		LOG_ERROR("Cannot emit particles: {} is not attached to an {}", NAMEOF(ParticleManager), NAMEOF(Entity));
+		return false;
+	}
+
+	shape = Parent()->Get<EmitterShape>();
+	if(shape == nullptr) {
+		LOG_ERROR("Cannot emit particles: {} is not attached to an {}", NAMEOF(EmitterShape), NAMEOF(Entity));
+		return false;
+	}
+
+	// Make sure all systems are available
+	if(IGlobalEventsSystem::Get() == nullptr) {
+		LOG_ERROR("Cannot emit particles: {} is not registered", NAMEOF(Systems::IGlobalEventsSystem));
+		return false;
+	}
+	if(ITimeSystem::Get() == nullptr) {
+		LOG_ERROR("Cannot emit particles: {} is not registered", NAMEOF(Systems::ITimeSystem));
+		return false;
+	}
+
+	// Bind to events
+	IGlobalEventsSystem::Get()->bind(Global::Update, &updateListener);
 	return true;
 }
 
-void ParticleEmitter::Update(float dt) {
-	// Grab manager component
-	ParticleManager *manager = Utils::GetComponentSafe<ParticleManager>(Parent(), LOG_MANAGER, PARTICLE_EMITTER);
-	if(manager == nullptr) {
-		return;
+const std::string_view &ParticleEmitter::NameClass() const {
+	static constexpr std::string_view className = NAMEOF(RassEngine::Components::Particles::ParticleEmitter);
+	return className;
+}
+
+bool ParticleEmitter::Read(Stream &stream) {
+	// Make sure stream is valid
+	if(!Component::Read(stream)) {
+		return false;
 	}
+
+	// Read the node values
+	stream.PushNode(NAMEOF(ParticleEmitter));
+
+	// Read the emitter properties, from "emitRate" to "scale".
+	stream.Read("EmitOnStart", isEmitting);
+	stream.Read("EmitRate", emitRate);
+	stream.Read("LifeTimeMin", lifetimeMin);
+	stream.Read("LifeTimeMax", lifetimeMax);
+	stream.Read("ScaleMin", scaleMin);
+	stream.Read("ScaleMax", scaleMax);
+
+	// Read the tintColor.
+	std::vector colorArgs = {1.f, 1.f, 1.f, 1.f};
+	if(stream.Read("TintColor", colorArgs) && (colorArgs.size() >= 4)) {
+		tintColor.r = colorArgs[0];
+		tintColor.g = colorArgs[1];
+		tintColor.b = colorArgs[2];
+		tintColor.a = colorArgs[3];
+	}
+
+	stream.ReadObject("Shape", [this, &stream] (const std::string &key) {
+		// Construct the object from the factory, first
+		Component *toConvert = IComponentFactory::Get()->Create(key);
+		if(toConvert == nullptr) {
+			LOG_ERROR("{}: unable to create object, {}", NAMEOF(ParticleEmitter), key);
+			return;
+		}
+
+		// See if this can be converted into a component
+		shape = dynamic_cast<Particles::EmitterShape *>(toConvert);
+		if(shape == nullptr) {
+			// Clean up the object
+			delete toConvert;
+
+			// Log that object wasn't a component
+			LOG_ERROR("{}: unable to create object, {}", NAMEOF(ParticleEmitter), key);
+			return;
+		}
+
+		// If so, read from the stream
+		shape->Read(stream);
+
+		// Add the shape as a component to the entity
+		Parent()->AddComponent(std::unique_ptr<Particles::EmitterShape>(shape));
+	});
+
+	// Return to the original location in the tree.
+	stream.PopNode();
+	return true;
+}
+
+#pragma endregion Public Functions
+
+#pragma region Private Functions
+
+bool ParticleEmitter::Update(const IEvent<Events::GlobalEventArgs> *, const Events::GlobalEventArgs &) {
+	float dt = ITimeSystem::Get()->GetDeltaTimeSec();
 
 	//----------------------------------------------------------------------------
 	// Spawn new particles.
 	//----------------------------------------------------------------------------
-
 	if(isEmitting) {
 		// Update the emit accumulator (accumulator += emitRate * dt).
 		emitAccumulator += emitRate * dt;
@@ -169,57 +216,7 @@ void ParticleEmitter::Update(float dt) {
 			Parent()->Destroy();
 		}
 	}
-}
-
-void ParticleEmitter::Read(Stream &stream) {
-	// Check for valid stream (optional).
-	DRAGON_ASSERT(stream.Contains(PARTICLE_EMITTER), "ParticleEmitter::Read: Failed to find required key: ParticleEmitter");
-
-	// Traverse down the tree to the "ParticleEmitter" object (PushNode).
-	stream.PushNode(PARTICLE_EMITTER);
-
-	// Read the emitter properties, from "emitRate" to "scale".
-	Utils::ReadOptionalAttribute(stream, "EmitOnStart", isEmitting, PARTICLE_EMITTER);
-	Utils::ReadOptionalAttribute(stream, "EmitRate", emitRate, PARTICLE_EMITTER);
-	Utils::ReadOptionalAttribute(stream, "LifeTimeMin", lifetimeMin, PARTICLE_EMITTER);
-	Utils::ReadOptionalAttribute(stream, "LifeTimeMax", lifetimeMax, PARTICLE_EMITTER);
-	Utils::ReadOptionalVector2D(stream, "ScaleMin", scaleMin, PARTICLE_EMITTER);
-	Utils::ReadOptionalVector2D(stream, "ScaleMax", scaleMax, PARTICLE_EMITTER);
-
-	// Read the tintColor.
-	std::vector colorArgs = {1.f, 1.f, 1.f, 1.f};
-	if(stream.Read("TintColor", colorArgs) && (colorArgs.size() >= 4)) {
-		tintColor.r = colorArgs[0];
-		tintColor.g = colorArgs[1];
-		tintColor.b = colorArgs[2];
-		tintColor.a = colorArgs[3];
-	}
-
-	stream.ReadObject("Shape", [this, &stream] (const std::string &key) {
-		// Construct the object from the factory, first
-		Object *toConvert = ObjectFactory::GetInstance().Create(key);
-		if(toConvert == nullptr) {
-			LoggingSystem::Debug("{}: unable to create object, {}", PARTICLE_EMITTER, key);
-			return;
-		}
-
-		// See if this can be converted into a component
-		shape = dynamic_cast<Particles::EmitterShape *>(toConvert);
-		if(shape == nullptr) {
-			// Clean up the object
-			delete toConvert;
-
-			// Log that object wasn't a component
-			LoggingSystem::Debug("{}: created object, \"{}\", was not a component", PARTICLE_EMITTER, key);
-			return;
-		}
-
-		// If so, read from the stream
-		shape->Read(stream);
-		});
-
-	// Return to the original location in the tree.
-	stream.PopNode();
+	return true;
 }
 
 void ParticleEmitter::SetupParticle(ParticleManager::StartingStats *startingStats, Particle *particle) {
@@ -229,15 +226,15 @@ void ParticleEmitter::SetupParticle(ParticleManager::StartingStats *startingStat
 	}
 
 	// Initialize the lifetime to a random value between lifetimeMin and lifetimeMax.
-	startingStats->lifetime = Random::Range(lifetimeMin, lifetimeMax);
+	startingStats->lifetime = Random::range(lifetimeMin, lifetimeMax);
 	particle->lifetime = startingStats->lifetime;
 
 	// Setup random color
-	startingStats->color = Color(Random::Range(0.f, tintColor.r), Random::Range(0.f, tintColor.g), Random::Range(0.f, tintColor.b), tintColor.a);
+	startingStats->color = glm::vec4(Random::range(0.f, tintColor.r), Random::range(0.f, tintColor.g), Random::range(0.f, tintColor.b), tintColor.a);
 	particle->color = startingStats->color;
 
 	// Set the particle's scale
-	startingStats->scale = TweenCurve::Calculate(TweenCurve::Type::Linear, scaleMin, scaleMax, Random::Range(0.f, 1.f));
+	startingStats->scale = TweenCurve::Calculate(TweenCurve::Type::Linear, scaleMin, scaleMax, Random::range(0.f, 1.f));
 	particle->scale = startingStats->scale;
 
 	// Confirm we have a shape
@@ -246,13 +243,13 @@ void ParticleEmitter::SetupParticle(ParticleManager::StartingStats *startingStat
 	}
 
 	// Get the parent entity's Transform.
-	const Transform *transform = Utils::GetComponentSafe<Transform>(Parent(), LOG_TRANSFORM, PARTICLE_EMITTER);
+	const Transform *transform = Parent()->GetTransform();
 	if(transform == nullptr) {
 		return;
 	}
 
 	// Initialize the particle's position and rotationRad
-	std::tie(particle->position, particle->rotationRad) = shape->GetEmitTransform(transform);
+	std::tie(particle->position, particle->rotationRad) = shape->GetEmitTransform(*transform);
 
 	// Compute the particle's velocities
 	std::tie(startingStats->velocity, startingStats->angularVelocityRad) = shape->GetInitVelocities(*particle);
